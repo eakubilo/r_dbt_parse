5b8422e4b0a7bfe3ea91584d4c15d01ac634a44a
Chukwbebuka Akubilo <eakubilo@wesleyan.edu>
2025-07-13 16:42:22 -0400

    ran readme and realized I need to figure out inheritence and typing

---
+++ src/dbt_common/dataclass_schema.rs
@@ -0,0 +1,12 @@
+
+
+trait dbtClassMixin{
+    """Convert and validate JSON schemas.
+
+    This trait adds methods to generate a JSON schema and
+    convert them to and from JSON encodable dicts with validation 
+    against the schema
+    """
+
+    const _mapped_fields : 
+}
